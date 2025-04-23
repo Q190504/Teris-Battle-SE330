a77cf5c12a03220a9d272b0d72a8034037e6c410
DanLinhHuynh-Niwashi <146794831+DanLinhHuynh-Niwashi@users.noreply.github.com>
2025-04-23 11:44:17 +0700

    Add client connection on main and help screen to handle message

---
+++ core/src/main/java/io/github/tetris_battle/HandleMessageScreen.java
@@ -0,0 +1,8 @@
+package io.github.tetris_battle;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.ScreenAdapter;
+
+public interface HandleMessageScreen extends Screen {
+    void HandleMessage(String msg);
+}
--- core/src/main/java/io/github/tetris_battle/Main.java
+++ core/src/main/java/io/github/tetris_battle/Main.java
@@ -1,11 +1,75 @@
 package io.github.tetris_battle;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+import io.github.network.client.ClientConnection;
+import io.github.network.server.NetworkManager;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Main extends Game {
+    public static ClientConnection client;
+    public static MatchScreen matchScreen;
+
+    private Timer reconnectTimer;
+
     @Override
     public void create() {
-        setScreen(new GameScreen());
+        connectToServer();
+
+        // Periodically check connection every 3 seconds
+        reconnectTimer = new Timer();
+        reconnectTimer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                if (client == null) {
+                    System.out.println("Disconnected. Attempting to reconnect...");
+                    reconnect();
+                }
+            }
+        }, 3000, 3000);
+
+        matchScreen = new MatchScreen(this);
+        setScreen(matchScreen);
+    }
+
+    public void connectToServer() {
+        try {
+            client = new ClientConnection("localhost", 5000, message -> {
+                System.out.println("From server: " + message);
+                if (screen instanceof HandleMessageScreen) {
+                    ((HandleMessageScreen) screen).HandleMessage(message);
+                }
+            });
+            client.start();
+
+            if (screen instanceof HandleMessageScreen) {
+                ((HandleMessageScreen) screen).HandleMessage("conn");
+            }
+
+        } catch (IOException e) {
+            client = null;
+            if (screen instanceof HandleMessageScreen) {
+                ((HandleMessageScreen) screen).HandleMessage("no_conn");
+            }
+            System.out.println("Failed to connect: " + e.getMessage());
+        }
+    }
+
+    public void reconnect() {
+        connectToServer();
+    }
+
+    public void SetScreen(Screen screen) {
+        setScreen(screen);
+    }
+
+    @Override
+    public void dispose() {
+        reconnectTimer.cancel();
+        if (client != null) client.close();
+        super.dispose();
     }
 }
