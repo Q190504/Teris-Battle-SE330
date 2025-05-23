36cb897b429effac4ddc6836ea3e2cf4bb801a5c
DanLinhHuynh-Niwashi <146794831+DanLinhHuynh-Niwashi@users.noreply.github.com>
2025-05-23 16:40:09 +0700

    Center app colors and messages to static vars

---
+++ core/src/main/java/io/github/ui/Messages.java
@@ -0,0 +1,36 @@
+package io.github.ui;
+
+public class Messages {
+    public static final String SEPARATOR = ":";
+    public static final String START = "start";
+    public static final String LEAVE = "leave";
+    public static final String ACCEPT = "accept";
+    public static final String JOIN_REQUEST = "join_request";
+    public static final String GAME_START = "game_start";
+    public static final String ROOM_CLOSED = "room_closed";
+    public static final String PLAYER_LEFT = "player_left";
+
+    public static final String CREATE = "create";
+    public static final String JOIN = "join";
+    public static final String AUTO = "auto";
+
+    public static final String READY = "ready";
+    public static final String UNREADY = "not_ready";
+
+    public static final String APPROVED = "approved";
+    public static final String REJECTED = "rejected";
+    public static final String ROOM_CREATED = "room_created";
+    public static final String NO_CONN = "no_conn";
+    public static final String CONN = "conn";
+    public static final String ROOM_FULL = "room_full";
+    public static final String ROOM_NOT_FOUND = "room_not_found";
+    public static final String REQUEST_PIECE = "request_piece";
+    public static final String PIECE = "piece";
+    public static final String NEXT_PIECE = "next_piece";
+    public static final String NOT_OWNER_OR_INVALID_ROOM = "not_owner_or_invalid_room";
+
+    // 💡 NEW MESSAGE TYPES FOR GAME LOGIC
+    public static final String GAME_STATE = "game_state";
+    public static final String LOCK_PLAYER = "lock_player";
+    public static final String UNLOCK_PLAYER = "unlock_player";
+}
